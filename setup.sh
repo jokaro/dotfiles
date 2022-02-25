6683d7a1a5ff98c8f7702b47ca06efaec71b2e4a
Joakim Karlsson <joakim.karlsson@sigmatechnology.se>
2022-02-25 14:16:42 +0100

    change

--- setup.sh
+++ setup.sh
@@ -4,20 +4,19 @@ cd $HOME
 sudo apt update
 
 # Install basetools
-
-# Install github-cli
-
+sudo apt install gzip unzip git gcc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
 
 # Install lastpass-cli
 sudo apt install lastpass-cli
 lpass login $1
 
-
-# Install chezmoi
-bash -c "$(curl -fsLS chezmoi.io/get)"
-
+# Install starship
+cargo install starship --locked
 
 # Install taskwarrior and timewarrior
 sudo apt install taskwarrior timewarrior
 
-# Install cargo install starship --locked
+# Install chezmoi
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply jokaro

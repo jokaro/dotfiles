1a1855dde2627916e3426737c21d33f09db57305
Joakim Karlsson <jocke@alloc.io>
2022-02-28 22:48:41 +0100

    nb

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ sudo apt install gzip unzip git gcc
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
+sudo apt install bat ripgrep
+
 # Install lastpass-cli
 sudo apt install lastpass-cli
 lpass login $1
@@ -20,3 +22,6 @@ sudo apt install taskwarrior timewarrior
 
 # Install chezmoi
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply jokaro
+
+# Setup nb
+nb init git@github.com:jokaro/notes.git --email jocke@alloc.io --name "Joakim Karlsson"

3a8ed1f60ddc02ad51e4947259a8de5f019467b0
Joakim Karlsson <joakim.karlsson@sigmatechnology.se>
2022-03-03 16:01:31 +0100

    tmux

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ sudo apt install gzip unzip git gcc
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
-sudo apt install bat ripgrep
+sudo apt install bat ripgrep ncat tig
+cargo install exa
 
 # Install lastpass-cli
 sudo apt install lastpass-cli
